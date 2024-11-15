d5aefaa827affbc26a51539bc91e2bf93cc09328
unknown <viniciusreis3105@gmail.com>
2024-11-15 12:01:12 -0300

    feat: add about dynamic content

---
+++ src/data/about.ts
@@ -0,0 +1,4 @@
+export const about = {
+	content:
+		"Com vasto conhecimento em desenvolvimento e UI/UX design, tenho me dedicado em desenvolvimento web em geral, como Landing Pages, E-commerces, Blogs, Dashboard etc. Atualmente minha especialidade é desenvolvimento Front-end. Resido em Paragominas/PA, onde atuo como freelancer.",
+};
